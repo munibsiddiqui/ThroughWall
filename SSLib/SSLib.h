12d5d0f593f29b61d66f88c419fd162b353329e7
Bin <bin@bins-macbook-pro.local>
2017-07-28 17:16:00 +0800

    add SSLib

---
+++ SSLib/SSLib.h
@@ -0,0 +1,19 @@
+//
+//  SSLib.h
+//  SSLib
+//
+//  Created by Bin on 28/07/2017.
+//  Copyright © 2017 Wu Bin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSLib.
+FOUNDATION_EXPORT double SSLibVersionNumber;
+
+//! Project version string for SSLib.
+FOUNDATION_EXPORT const unsigned char SSLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSLib/PublicHeader.h>
+
+
